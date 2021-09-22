a5885ec198f393dd073f3f5c422c9ee897009e8f
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-09-23 00:03:35 +0700

    Ввод даты

--- Date.cpp
+++ Date.cpp
@@ -65,16 +65,69 @@ void Date::ReadTime()
 		else if (digit == '\b' && i > 0)
 		{
 			i -= 2;
-			if (i % 2 == 0)
+			if (i % 2 == 0 && i < 4)
 				printf("\b \b");
 			printf("\b \b");
 
-			if (i == -1)
-				printf(" ");
 		}
 		else
 			i--;
 	}
+
+	hour = time[0] * 10 + time[1] - 528;
+	min = time[2] * 10 + time[3] - 528;
+	sec = time[4] * 10 + time[5] - 528;
+
+	return;
+}
+
+//¬вод даты
+void Date::ReadDate()
+{
+	char date[8], digit;
+
+	printf("¬ведите дату (в формате YYYY.MM.DD): ");
+	for (int i = 0; i < 9; i++)
+	{
+		digit = _getch();
+		if (i == 8 && digit == '\r')
+			i++;
+		else if (i < 8 && digit >= '0' && digit <= '9' &&
+			(i == 0 && digit <= '2' ||
+				i == 1 && (digit == '0' || date[0] <= '1') ||
+				i == 2 ||
+				i == 3 && (digit >= '1' || digit == '0' && (date[0] != '0' || date[1] != '0' || date[2] != '0')) ||
+				i == 4 && digit <= '1' ||
+				i == 5 && (date[4] == '0' && digit >= '1' || date[4] == '1' && digit <= '2') ||
+				i == 6 && (digit <= '2' || (date[4] != '0' || date[5] != '2') && digit == '3') ||
+				i == 7 && (digit == '0' && date[6] != '0' || 
+					digit == '1' && (date[6] <= '2' || date[4] == '0' && (date[5] == '1' || date[5] == '3' || date[5] == '5' || date[5] == '7' || date[5] == '8') || date[4] == '1' && (date[5] == '0' || date[5] == '2')) ||
+					digit >= '2' && digit <= '8' && date[6] <= '2' ||
+					digit == '9' && (date[6] <= '2' && (date[4] != '0' || date[5] != '2') || (date[6] <= '1' || date[6] == '2' && (date[2] * 10 + date[3]) % 4 == 0)))))
+		{
+			date[i] = digit;
+			printf("%c", digit);
+			if (i % 2 == 1 && i < 7 && i > 2)
+				printf(".");
+		}
+		else if (digit == '\b' && i > 0)
+		{
+			i -= 2;
+			if (i % 2 == 0 && i > 0 && i < 6)
+				printf("\b \b");
+			printf("\b \b");
+
+		}
+		else
+			i--;
+	}
+
+	year = 0;
+	for (int i = 0; i < 4; i++)
+		year += (date[i] - 48) * pow(10, 3 - i);
+
+	month = date[4] * 10 + date[5] - 528;
+	day = date[6] * 10 + date[7] - 528;
 
 	return;
 }
@@ -95,8 +148,9 @@ void Date::Init(int seconds, int minutes, int hours, int day, int month, int yea
 void Date::Read() 
 {
 	ReadTime();
-	scanf("%d %d %d %d %d %d", &hour, &min, &sec, &day, &month, &year);
-	while (getchar() != '\n');
+	printf("\n");
+	ReadDate();
+	Display();
 	return;
 }
 
